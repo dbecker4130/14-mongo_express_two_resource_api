1f7aea5fced22133ab8958715cda387f43c0cdf6
Daniel Becker <dbecker4130@gmail.com>
2017-01-03 17:31:13 -0800

    artist route tests complete, added put and delete, added bad request tests

--- test/artist-route-test.js
+++ test/artist-route-test.js
@@ -53,5 +53,154 @@ describe('Artist Routes', function() {
         });
       });
     });
+    describe('with an invalid LibraryID', () => {
+      it('should return a 404 status code', done => {
+        request.post(`${url}/api/library/0000/artist`)
+        .send(exampleArtist)
+        .end((err, res) => {
+          expect(err).to.be.an('error');
+          expect(res.status).to.equal(404);
+          done();
+        });
+      });
+    });
+    describe('with an invalid artist body', () => {
+      it('should return a 400 status code', done => {
+        request.post(`${url}/api/library/${this.tempLibrary._id}/artist`)
+        .send({name: null})
+        .end((err, res) => {
+          expect(err).to.be.an('error');
+          expect(res.status).to.equal(400);
+          done();
+        });
+      });
+    });
+  });
+  describe('GET: api/artist/:id', function() {
+    describe('with a valid body', function() {
+      before( done => {
+        exampleLibrary.timestamp = new Date();
+        new Library(exampleLibrary).save()
+        .then( library => {
+          this.tempLibrary = library;
+          return Library.findByIdAndAddArtist(library._id, exampleArtist);
+        })
+        .then( artist => {
+          this.tempArtist = artist;
+          done();
+        })
+        .catch(done);
+      });
+      after( done => {
+        Promise.all([
+          Library.remove({}),
+          Artist.remove({})
+        ])
+        .then( () => done())
+        .catch(done);
+      });
+      it('should return an artist', done => {
+        request.get(`${url}/api/artist/${this.tempArtist._id}`)
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.name).to.equal('test artist name');
+          expect(res.body.genre).to.equal('test artist genre');
+          done();
+        });
+      });
+    });
+    describe('invalid get request', function() {
+      it('should throw a 404 status code', done => {
+        request.get(`${url}/api/artist/`)
+        .end((err, res) => {
+          expect(err).to.be.an('error');
+          expect(res.status).to.equal(404);
+          done();
+        });
+      });
+    });
+  });
+  describe('PUT: /api/artist/:id', function() {
+    describe('with a valid body', function() {
+      before( done => {
+        new Artist(exampleArtist).save()
+        .then( artist => {
+          this.tempArtist = artist;
+          done();
+        })
+        .catch(done);
+      });
+      after( done => {
+        if(this.tempArtist) {
+          Artist.remove({})
+          .then( () => done())
+          .catch(done);
+          return;
+        }
+        done();
+      });
+      it('should update by id', done => {
+        request.put(`${url}/api/artist/$(this.tempArtist._id)`)
+        .send({name:'name', genre:'genre'})
+        .end((err, res) => {
+          if(err) return done(err);
+          expect(res.status).to.equal(200);
+          expect(res.body.name).to.equal('name');
+          expect(res.body.genre).to.equal('genre');
+          done();
+        });
+      });
+    });
+    describe('bad put request', function() {
+      it('should thorw a 404 status code', done => {
+        request.put(`${url}/api/artist/`)
+        .send({name:'name', genre:'genre'})
+        .end((err, res) => {
+          expect(err).to.be.an('error');
+          expect(res.status).to.equal(404);
+          done();
+        });
+      });
+    });
+  });
+  describe('DELETE: /api/artist/:id', function() {
+    describe('with a valid body', function() {
+      before(done => {
+        new Artist(exampleArtist).save()
+        .then( artist => {
+          this.tempArtist = artist;
+          done();
+        })
+        .catch(done);
+      });
+      after( done => {
+        if(this.tempArtist) {
+          Artist.remove({})
+          .then(() => done())
+          .catch(done);
+          return;
+        }
+        done();
+      });
+      it('should delete an artist', done => {
+        request.delete(`${url}/api/artist/${this.tempArtist._id}`)
+        .end((err, res) => {
+          if(err) return done(err);
+          expect(res.status).to.equal(204);
+          done();
+        });
+      });
+    });
+    describe('bad delete request', function() {
+      it('should throw a 404 status code', done => {
+        request.delete(`${url}/api/artist`)
+        .end((err, res) => {
+          expect(err).to.be.an('error');
+          expect(res.status).to.equal(404);
+          done();
+        });
+      });
+    });
   });
 });
